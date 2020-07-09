225333382f6a158af8bb3499fffd2712e30fdb15
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-07-09 13:53:50 +0900

    first commit

---
+++ src/main/java/jp/co/rakus/GitBbsApplication.java
@@ -0,0 +1,13 @@
+package jp.co.rakus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBbsApplication.class, args);
+	}
+
+}
