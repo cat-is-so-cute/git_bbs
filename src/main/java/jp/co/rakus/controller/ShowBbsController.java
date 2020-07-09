da438411d9ed2ddfddd1e2d38e61d2df127e7089
森本 義基 <yoshiki.morimoto@rakus.internal>
2020-07-09 15:12:03 +0900

    ShowBbsとInsertArticleを実装

---
+++ src/main/java/jp/co/rakus/controller/ShowBbsController.java
@@ -0,0 +1,28 @@
+package jp.co.rakus.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.rakus.domain.Article;
+import jp.co.rakus.repository.ArticleRepository;
+
+@Controller
+@RequestMapping("")
+public class ShowBbsController {
+	@Autowired
+	private ArticleRepository repository;
+	
+	public String index(Model model) {
+		List<Article> articleList = repository.findAll();
+//		for (Article article : articleList) {
+//			List<Comment> commentList = commentRepository.findByArticledId(article.getId());
+//			article.setCommentList(commentList);
+//		}
+		model.addAttribute("articleList", articleList);
+		return "bbs";
+	}
+}
